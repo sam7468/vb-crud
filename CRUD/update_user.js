2620151cfb321ffbfc5794a7a6ec31bcae0f0019
sam7468 <75119000+sam7468@users.noreply.github.com>
2021-10-06 22:22:14 +0530

    Update update_user.js

--- CRUD/update_user.js
+++ CRUD/update_user.js
@@ -4,7 +4,7 @@ MongoClient.connect(url, function (err, client) {
    if (err) throw err;
 
    const db = client.db("user_managements");
-   db.collection("users").findOneAndUpdate({_id:"12"},{$set : {"f_name":"sam"} },function (err, result) {
+   db.collection("users").updateOne({_id:"12"},{$set : {"f_name":"sam"} },function (err, result) {
      if (err) throw err;
      console.log("updated document: " + JSON.stringify(result.value));
      client.close();
